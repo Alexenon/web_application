a36e24755f00e51a06afc8c739f81af6a1860b3d
Alex Xenon <b3haply@gmail.com>
2022-07-19 20:20:58 +0300

    Prepared everything for sending a post http request

--- src/main/java/com/xenon/web_application/service/StudentService.java
+++ src/main/java/com/xenon/web_application/service/StudentService.java
@@ -1,4 +1,19 @@
 package com.xenon.web_application.service;
 
+import com.xenon.web_application.model.Student;
+import com.xenon.web_application.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository repository;
+
+    /* TODO: Think about made this method return type to be void */
+    public Student saveStudent(Student student) {
+        return repository.save(student);
+    }
+
 }
