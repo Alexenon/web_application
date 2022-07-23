be40276e6d8d3a1d8babd194c6eb8331f2639200
Alex Xenon <b3haply@gmail.com>
2022-07-23 17:21:20 +0300

    Finished JSON getter for a certain student by name

--- src/main/java/com/xenon/web_application/service/StudentService.java
+++ src/main/java/com/xenon/web_application/service/StudentService.java
@@ -5,16 +5,24 @@ import com.xenon.web_application.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
 
     @Autowired
     StudentRepository repository;
 
-    @SuppressWarnings("UnusedReturnValue")
     public Student saveStudent(Student student) {
-        System.out.println("Student successfully saved!");
         return repository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+
+    public Student getStudentByName(String name) {
+        return repository.findStudentByName(name);
     }
 
 }
