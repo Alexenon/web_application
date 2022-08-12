f02efd755363bb20b54a334fdb62b0d75c7ffeac
Alex Xenon <b3haply@gmail.com>
2022-08-12 17:05:45 +0300

    Refactor naming for not-found page

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
 import Collections from "./pages/collections/Collections";
-import NotFound from "./pages/not-found-page/Not-found-page";
+import NotFound from "./pages/not-found/NotFound";
 
 ReactDOM.render(
     <Router>
